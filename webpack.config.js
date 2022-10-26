178f07d05ee7db216a4dc0dda843f7329bea1b31
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-10-26 15:40:41 +0200

    Checking linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-
+  mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-
   plugins: [
     new HtmlWebpackPlugin({
-    title: 'Development',
+      template: './src/index.html',
     }),
   ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  devServer: {
+    static: './dist',
   },
   module: {
     rules: [
@@ -26,10 +19,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
