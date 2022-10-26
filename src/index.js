a92ef162005087798f60a29ae6c75357eb2f9dbe
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-10-26 15:49:21 +0200

    Linter errors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
+const items = async () => {
+  try {
+    let resorve = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/search.php?f=e'
+    );
+    console.log(resorve);
 
-
-const items = async () =>{
-    try {
-        let resorve = await fetch ('https://www.themealdb.com/api/json/v1/1/search.php?f=e')
-        console.log (resorve)
-
-        let data = await resorve.json();
-        console.log (data)
-        meals = data.meals
-    meals.forEach(element => {
-        document.getElementById('recipe-section').innerHTML +=`<p>${element.strMeal}</p>
-        <img src=${element.strMealThumb} alt="img=" class="strMealThumb">`
+    let data = await resorve.json();
+    console.log(data);
+    meals = data.meals;
+    meals.forEach((element) => {
+      document.getElementById(
+        'recipe-section'
+      ).innerHTML += `<p>${element.strMeal}</p>
+        <img src=${element.strMealThumb} alt="img=" class="strMealThumb">`;
     });
-    } catch (error) {
-        return error;
-    }
-    }
+  } catch (error) {
+    return error;
+  }
+};
 
-    items();
-
+items();
