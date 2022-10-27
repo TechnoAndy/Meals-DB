a490e3f75efef7e4bfbc3a29001521d3d6b81daa
henry <88551100+henrykgggg@users.noreply.github.com>
2022-10-27 10:58:09 -0700

    added homepage list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './style.css';
+const items = async () => {
+  try {
+    const resorve = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=e');
+    const data = await resorve.json();
+    const { meals } = data;
+    meals.forEach((element) => {
+      document.getElementById('recipe-section').innerHTML += `<p>${element.strMeal}</p>
+          <img src=${element.strMealThumb} alt="img=" class="images">`;
+    }); return data;
+  } catch (error) {
+    return error;
+  }
+};
+items();
