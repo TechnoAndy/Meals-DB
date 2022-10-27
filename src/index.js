c9e7a96e34b1092e699cb2e689275497c119743e
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-10-27 20:12:46 +0200

    Added styling and imported data for popup from api

--- src/index.js
+++ src/index.js
@@ -8,11 +8,35 @@ const items = async () => {
       data.meals.forEach((element) => {
         document.getElementById(
           'recipe-section',
-        ).innerHTML += `<div class="card">
+        ).innerHTML = `<div class="card">
             <h1 class="title">${element.strMeal}</h1>
             <img src=${element.strMealThumb} alt="img" class="images">
             <button class='comment-btn' type='button'>Comments</button>
-         </div>`;
+         </div>
+         <div class="popup-window">
+<div class="close-btn">
+  <i id="close" class="fa-solid fa-xmark"></i>
+</div>
+<div class="popup-images">
+  <img src="${element.strMealThumb}" alt="popup-image" />
+</div>
+<div class="item-info">
+  <h2 class="dish-name">Dish name: ${element.strMeal}</h2>
+  <h2 class="origin">Origin: ${element.strArea}</h2>
+  <h2 class="Category">Category:${element.strCategory}</h2>
+  <h2 class="recipe">Instructions${element.strInstructions}</h2>
+</div>
+<div class="comments-container">
+  <h4 class="cmt-heading">Add a comment</h4>
+  <input type="text" class="comment-name" placeholder="Your Name"><br>
+  <input type="text" class="comment-content" placeholder="Your Comment"><br>
+  <button class="add-comment">Submit</button>
+  </div>
+  <span id="comments-spinner" class="spinner">Comments</span>
+  <div class="comment-holder">
+  </div>
+</div>
+         `;
       });
     }); return 1;
   } catch (error) {
