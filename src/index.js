ff12fd8b4498457cd6ab19be9b2fcbdc8e1beb87
henry <88551100+henrykgggg@users.noreply.github.com>
2022-10-27 11:40:55 -0700

    removed linter error

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import './style.css';
+
 const items = async () => {
-    try {
-      fetch(
-        "https://www.themealdb.com/api/json/v1/1/search.php?f=e"
-      ).then((res) => res.json()).then((data) => {
-        data.meals.forEach((element) => {
-          document.getElementById(
-            "recipe-section"
-          ).innerHTML += `<p>${element.strMeal}</p>
+  try {
+    fetch(
+      'https://www.themealdb.com/api/json/v1/1/search.php?f=e',
+    ).then((res) => res.json()).then((data) => {
+      data.meals.forEach((element) => {
+        document.getElementById(
+          'recipe-section',
+        ).innerHTML += `<p>${element.strMeal}</p>
             <img src=${element.strMealThumb} alt="img=" class="images">`;
-            console.log(element.strMeal)
-        });
-      })
+      });
+    }); return 1;
+  } catch (error) {
+    return error;
+  }
+};
 
-    } catch (error) {
-      return error;
-    }
-  };
-
-  items();
+items();
