a29e6443e352d8bb6f6c6f2d047dbead6776fdf1
henry <88551100+henrykgggg@users.noreply.github.com>
2022-10-27 11:32:10 -0700

    added comment button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+import './style.css';
 const items = async () => {
-  try {
-    const resorve = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=e');
-    const data = await resorve.json();
-    const { meals } = data;
-    meals.forEach((element) => {
-      document.getElementById('recipe-section').innerHTML += `<p>${element.strMeal}</p>
-          <img src=${element.strMealThumb} alt="img=" class="images">`;
-    }); return data;
-  } catch (error) {
-    return error;
-  }
-};
-items();
+    try {
+      fetch(
+        "https://www.themealdb.com/api/json/v1/1/search.php?f=e"
+      ).then((res) => res.json()).then((data) => {
+        data.meals.forEach((element) => {
+          document.getElementById(
+            "recipe-section"
+          ).innerHTML += `<p>${element.strMeal}</p>
+            <img src=${element.strMealThumb} alt="img=" class="images">`;
+            console.log(element.strMeal)
+        });
+      })
+
+    } catch (error) {
+      return error;
+    }
+  };
+
+  items();
